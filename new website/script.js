199d86fa88ea351ade8bcdad2aa02ce26b5ececc
vineet1072 <90325407+vineet1072@users.noreply.github.com>
2022-02-09 18:33:50 +0530

    Add files via upload

---
+++ new website/script.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
+
+
+
+btn.addEventListener('click', ()=> {
+    overlay.style.display = 'grid';
+    overlay.classList.add('animate-overlay')
+
+  
+    
+})
+
